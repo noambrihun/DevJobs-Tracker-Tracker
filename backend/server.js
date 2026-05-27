41af59332038fd350a3b8d44d175356ebc14c8a5
noambrihun <noambrihun1@gmail.com>
2026-05-27 22:58:42 +0300

    first commit for this  DevJobs Tracker fullstack project

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Mongo connected"))
+  .catch(err => console.log("Mongo connection error", err))
+
+console.log("MONGO_URI", process.env.MONGO_URI)
+app.use(cors())
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server works " })
+})
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`)
+})
