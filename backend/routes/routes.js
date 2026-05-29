79f4602e449962b0d378b73982fd45c9e986c3db
noambrihun <noambrihun1@gmail.com>
2026-05-29 14:49:26 +0300

    I added post route

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -7,10 +7,26 @@ res.send("jobs are working")
 
 })
 
-Router.post("/", (req, res) => {
+Router.post("/", async (req, res) => {
     const { company, position, status, salary, link, notes } = req.body
-    const newJob = new Job({ company, position, status, salary, link, notes })
-    newJob.save()
-    res.status(201).json(newJob)
+    try{
+        const newJob = new Job({ company, position, status, salary, link, notes })
+        await newJob.save()
+        res.status(201).json({ message: "Job created", newJob })
+    }catch(e){
+        res.status(500).json({ message: "Error creating job", error: e.message })
+    }
+})
+
+Router.delete("/:id", async (req,res) => {
+    try{
+        const deletedJob = await Job.findByIdAndDelete(req.params.id)
+        if(!deletedJob){
+            return res.status(404).json({ message: "Job not found" })
+        }
+        res.status(200).json({ message: "Job deleted", deletedJob })
+    }catch(e){
+        res.status(500).json({ message: "Error deleting job", error: e.message })
+    }
 })
 module.exports = Router;
